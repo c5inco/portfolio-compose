493f6b56acc56ffef92327f65de6fd58aea4cd74
Chris Sinco <csinco@google.com>
2021-05-06 09:39:39 -0700

    Move home screen back

--- src/jsMain/kotlin/com/c5inco/portfolio/Main.kt
+++ src/jsMain/kotlin/com/c5inco/portfolio/Main.kt
@@ -2,6 +2,7 @@ package com.c5inco.portfolio
 
 import androidx.compose.web.css.Style
 import androidx.compose.web.renderComposable
+import com.c5inco.portfolio.screens.HomeScreen
 import com.c5inco.portfolio.screens.ProjectsScreen
 import com.c5inco.portfolio.style.AnimateCssStyleSheet
 import com.c5inco.portfolio.style.CommonStylesheet
@@ -13,8 +14,8 @@ fun main() {
         Style(AnimateCssStyleSheet)
         Style(CommonStylesheet)
 
-        //HomeScreen()
-        ProjectsScreen()
+        HomeScreen()
+        // ProjectsScreen()
     }
 }
 
